aafe8bc94ecd79a31ecd5f4d627642874acef72f
lln <806064176@qq.com>
2024-05-21 17:33:28 +0800

    提交unittest

---
+++ main.py
@@ -0,0 +1,20 @@
+import unittest
+import os
+
+# from BeautifulReport import BeautifulReport
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+ENVIRON = 'Online'  # 'Online' -> 线上环境， ‘Offline’-> 测试环境
+
+if __name__ == '__main__':
+    # run_pattern = 'all'  # all 全量测试用例执行 /smoking 冒烟测试执行 / 指定执行文件
+    # if run_pattern == 'all':
+    #     pattern = 'test_*.py'
+    # elif run_pattern == 'smoking':
+    #     pattern = 'test_major*.py'
+    # else:
+    #     run_pattern = run_pattern + '.py'
+    suite = unittest.defaultTestLoader.discover(start_dir='./testcases', pattern='test_major*.py')
+    unittest.TextTestRunner().run(suite)
+    # result = BeautifulReport(suite)
+    # result.report(filename='report.html', description='测试报告', report_dir='./')
